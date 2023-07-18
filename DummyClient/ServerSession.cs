d96fb7c976cd73ccceeb56438cd36f96bb3f0ce0
tjdqja0806 <74092254+tjdqja0806@users.noreply.github.com>
2023-07-18 10:36:30 +0900

    First Upload
    Upload All

---
+++ DummyClient/ServerSession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ServerCore;
+
+namespace DummyClient
+{
+    class ServerSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+        }
+
+        public override void OnDisConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisConnected : {endPoint}");
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            //Console.WriteLine($"Transferred bytes : {numOfBytes}");
+        }
+    }
+
+}
---
+++ Server/Session/ClientSession.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+using ServerCore;
+
+namespace Server
+{
+    class ClientSession : PacketSession
+    {
+        public int SessionId { get; set; }
+        public GameRoom Room { get; set; }
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
+
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            Program.Room.Push(() => Program.Room.Enter(this));
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
+
+        public override void OnDisConnected(EndPoint endPoint)
+        {
+            SessionManager.Instance.Remove(this);
+            if(Room != null)
+            {
+                GameRoom room = Room;
+                room.Push(() => room.Leave(this));
+                Room = null;
+            }
+
+            Console.WriteLine($"OnDisConnected : {endPoint}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            //Console.WriteLine($"Transferred bytes : {numOfBytes}");
+        }
+    }
+
+}
